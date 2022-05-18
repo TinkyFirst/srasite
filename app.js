4c4940d605d7f7f4aa50c15ca275c2d420f63254
AndreyPC <andreypc@MacBook-Pro.local>
2022-05-18 17:24:41 +0300

    fix

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sv.js').then(function(registration) {
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  }).catch(function(err) {
-    //registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}else {
-  console.log('No service-worker on this browser');
-}
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('sv.js').then(function(registration) {
+//     console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//   }).catch(function(err) {
+//     //registration failed :(
+//     console.log('ServiceWorker registration failed: ', err);
+//   });
+// }else {
+//   console.log('No service-worker on this browser');
+// }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
